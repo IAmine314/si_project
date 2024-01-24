7afe3165885ed3d23e97481233312e71bb5fac27
Ixlammm <islamtazerout3@gmail.com>
2024-01-24 21:05:38 +0100

    Added patient, dossiermedical, clinique, rendezvous, departement, service, tache, equipe, medecin models

--- clinique/admin.py
+++ clinique/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+
+admin.site.register(Patient)
+admin.site.register(DossierMedical)
+admin.site.register(RendezVous)
+admin.site.register(Equipe)
+admin.site.register(Tache)
+admin.site.register(Medecin)
+admin.site.register(Service)
+admin.site.register(Departement)
+admin.site.register(Clinique)
--- clinique/models.py
+++ clinique/models.py
@@ -1,3 +1,39 @@
 from django.db import models
 
-# Create your models here.
+class Patient(models.Model):
+    NSS = models.CharField(max_length=16, primary_key=True)
+    Nom = models.CharField(max_length=30)
+    Prenom = models.CharField(max_length=30)
+    DateDeNaissance = models.DateField(auto_now=True)
+
+class DossierMedical(models.Model):
+    NumDossier = models.IntegerField(primary_key=True, auto_created=True)
+
+class Clinique(models.Model):
+    NomClinique = models.CharField(max_length=30, primary_key=True)
+    Adresse = models.CharField(max_length=200)
+
+class RendezVous(models.Model):
+    NumRdv = models.IntegerField(primary_key=True, auto_created=True)
+    DateRdv = models.DateField()
+    Cause = models.CharField(max_length=200)
+
+class Departement(models.Model):
+    NomDepartement = models.CharField(max_length=20, primary_key
+                                      =True)
+    
+class Service(models.Model):
+    NumService = models.IntegerField(primary_key=True, auto_created=True)
+
+class Tache(models.Model):
+    NumTache = models.IntegerField(primary_key=True, auto_created=True)
+
+class Equipe(models.Model):
+    NumEquipe = models.IntegerField(primary_key=True, auto_created=True)
+
+class Medecin(models.Model):
+    Nom = models.CharField(max_length=30, primary_key=True)
+    Prenom = models.CharField(max_length=30)
+    Specialite = models.CharField(max_length=20)
+
+
