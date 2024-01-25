001373b93e4a58404a200ede94ca6f83bd01a818
Ixlammm <islamtazerout3@gmail.com>
2024-01-25 23:43:58 +0100

    Added few css styles

--- si_project/urls.py
+++ si_project/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.shortcuts import render
+from clinique.views import *
 
 urlpatterns = [
+    path('test/', test_view),
     path('admin/', admin.site.urls),
 ]
